4783315ff90da795cfc18e7bf1e2686faba21534
Isaac Shapira <isaac.shapira@logrhythm.com>
2015-10-14 11:41:08 -0600

    basic works again

--- src/Http/Server/Simple.elm
+++ src/Http/Server/Simple.elm
@@ -3,7 +3,6 @@ module Http.Server.Simple where
 import Http.Server as Raw
 import Html
 import Json.Encode as Json
-import Effects exposing (Never)
 import Task exposing (Task, andThen)
 import VDOMtoHTML exposing (toHTML)
 import Signal exposing (..)
@@ -28,7 +27,7 @@ marshallRequest req =
     (Raw.method req)
     (Raw.statusCode req)
 
-runResponse : Response -> Raw.Response -> Task Never ()
+runResponse : Response -> Raw.Response -> Task x ()
 runResponse res rawres = case res of
   Html vdom -> Raw.writeHtml rawres (toHTML vdom)
   Json json -> Raw.writeJson rawres json
@@ -38,18 +37,18 @@ runResponse res rawres = case res of
 type alias Server model = Signal Request -> Signal (model, Response)
 type alias Port = Raw.Port
 
-run : Port -> Server model -> Signal (Task Never ())
+run : Port -> Server model -> Signal (Task x ())
 run p s = let
 
   server : Mailbox (Raw.Request, Raw.Response)
   server = mailbox (Raw.emptyReq, Raw.emptyRes)
 
-  reply : Signal (Task Never ())
+  reply : Signal (Task x ())
   reply = (runResponse << snd)
     <~ s (marshallRequest <~ (fst <~ server.signal))
      ~ (snd <~ server.signal)
 
-  create : Signal (Task Never ())
+  create : Signal (Task x ())
   create = constant <|
     Raw.createServer'
       server.address p ("Listening on " ++ toString p)
